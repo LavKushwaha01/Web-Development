a016006813639ea6dd157e1c0bb230a85e4a3808
lav kumar <lavkumar062@gmail.com>
2024-11-24 11:46:58 +0530

    settimeout

---
+++ Week 2/Async_callback/setTtimeout.js
@@ -0,0 +1,16 @@
+
+
+console.log("hii");  //1st 
+
+function timeout(){
+    console.log("this is timeout here");
+}
+
+setTimeout(timeout,5000);   //3rd  
+let x=0;
+for(let i=0; i<11000000; i++){
+    x++;               //2nd
+}
+console.log(x);
+
+Console.log("done");   //4th
