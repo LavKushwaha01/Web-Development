8a4bc9a988975549736c87b37bc90f2a623d83f7
lav kumar <lavkumar062@gmail.com>
2025-01-18 23:12:56 +0530

    todo project

--- Week 3/dom(simple)/todo_project/script.js
+++ Week 3/dom(simple)/todo_project/script.js
@@ -17,6 +17,6 @@ function deleteTodo(){
     if(num.value>i){
         alert("please enter number in range of todo")
     }
-    const element = document.getElementById("todo-"+num.value)
-    element.parentNode.removeChild(element);
+    const element = document.getElementById("todo-"+num.value);
+    element.parentNode.removeChild(element); 
 }
